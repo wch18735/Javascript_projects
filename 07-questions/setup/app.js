9601324f9936f4f6ac63195ad3f12ab0ff89bce6
wch18735 <wch18735@naver.com>
2021-04-08 00:07:21 +0900

    question with selectall and forEach

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function(){
+        // check to duplicate
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove('show-text');
+            }
+        });
+
+        // toggle
+        question.classList.toggle('show-text');
+    });
+});
+
+
 // traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e)
+//     {
+//         const parentQeustion = e.currentTarget.parentElement.parentElement;
+//         parentQeustion.classList.toggle("show-text");
+//     });
+// });
