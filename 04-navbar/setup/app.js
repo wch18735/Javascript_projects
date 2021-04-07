833de97533c1e73eeae9b028a4223fcf70d5c653
wch18735 <wch18735@naver.com>
2021-04-07 19:06:00 +0900

    nav bar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -2,4 +2,18 @@
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
-// toggle - toggles class
+// toggle - toggles class -> same operation
+
+const navToggle = document.querySelector(".nav-toggle");
+const Links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function(){
+    // if(!Links.classList.contains('show-links'))
+    // {
+    //     Links.classList.add('show-links');
+    // } else{
+    //     Links.classList.remove('show-links');
+    // }
+
+    Links.classList.toggle("show-links");
+});
